3a15bfe56d00314cfd5c38ed2e83f9a3d96d6737
Aryan Singh <aryansingh920@outlook.com>
2024-12-14 23:36:03 +0000

    init

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import { ProjectSection } from "@/types/types";
 
@@ -12,35 +12,30 @@ export default async function BlogPage({
     return <h1>Invalid Route</h1>;
   }
 
-  const jsonPath = path.join(process.cwd(), "public", "projects.json");
-  const rawData = fs.readFileSync(jsonPath, "utf-8");
-  const data: ProjectSection[] = JSON.parse(rawData);
-
-  console.log("Parsed JSON Data:", data);
-
-  // Find the project path dynamically
-  const projectPath = data
-    .flatMap((section) => section.projects)
-    .find((project) => project.name === params.slug)?.path;
-
-  if (!projectPath) {
-    return <h1>Project not found</h1>;
-  }
-
-  const filePath = path.join(process.cwd(), projectPath, "index.html");
-  console.log("Resolved File Path:", filePath);
-
-  let htmlContent = "";
   try {
-    htmlContent = fs.readFileSync(filePath, "utf-8");
-  } catch (error) {
-    console.error("Error reading file:", error);
-    return <h1>Project not found</h1>;
-  }
+    const jsonPath = path.join(process.cwd(), "public", "projects.json");
+    const rawData = await fs.readFile(jsonPath, "utf-8");
+    const data: ProjectSection[] = JSON.parse(rawData);
 
-  return (
-    <div className="p-1">
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-    </div>
-  );
+    // Find the project path dynamically
+    const projectPath = data
+      .flatMap((section) => section.projects)
+      .find((project) => project.name === params.slug)?.path;
+
+    if (!projectPath) {
+      return <h1>Project not found</h1>;
+    }
+
+    const filePath = path.join(process.cwd(), projectPath, "index.html");
+    const htmlContent = await fs.readFile(filePath, "utf-8");
+
+    return (
+      <div className="p-1">
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error in BlogPage component:", error);
+    return <h1>An error occurred</h1>;
+  }
 }
