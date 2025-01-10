c72dbb311a087ddd8a9fcbbd300146c720b8444e
Aryan Singh <aryansingh920@outlook.com>
2025-01-10 00:42:55 +0000

    Enhance layout and styling of home and section pages with banner and profile images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,9 +97,46 @@ export default function Home() {
       }
     >
       <section>
-        <h1 className="text-3xl font-bold mb-1 mt-1">{"All Blog's"}</h1>
+        <div className="max-w rounded-lg ">
+          <div className="relative">
+            <img
+              src="https://media.licdn.com/dms/image/v2/D5616AQF12usVORj44g/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1735963984496?e=1741824000&v=beta&t=0fflCZctRzBVR-E4KWnrM4_yPRVgOW3kZx1MVfgk8B4"
+              alt="Banner"
+              className="w-full  object-cover rounded-t-lg"
+            />
+            {/* <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 text-white font-bold">
+              <span className="text-sm">With banner image preview</span>
+            </div> */}
+          </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="relative flex justify-center">
+            <div className="absolute -top-10">
+              <img
+                src="/profile.jpeg"
+                alt="Profile"
+                className="w-20 h-20 rounded-full border-4 border-white"
+              />
+            </div>
+          </div>
+
+          <div className="mt-10 text-center">
+            <h1 className="text-3xl  font-semibold">{"All my work's"}</h1>
+            {/* <div className="flex justify-center mt-4 space-x-2">
+              <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                Message
+              </button>
+              <button className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400">
+                Call
+              </button>
+            </div> */}
+          </div>
+        </div>
+        {/* </section>
+
+      <section> */}
+        {/* <h1 className="text-3xl font-bold mb-1 mt-1">{"All Blog's"}</h1> */}
+
+        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {shuffledProjects
             .reverse()
             // .sort(() => Math.random() - 0.5)
